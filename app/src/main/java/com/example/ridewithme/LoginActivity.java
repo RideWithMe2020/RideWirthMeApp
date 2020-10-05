907fd85a9a2e6163453da12a0de4348c2e6a11be
johny <yonatani94@gmail,com>
2020-10-05 19:05:27 +0300

    johny

--- app/src/main/java/com/example/ridewithme/LoginActivity.java
+++ app/src/main/java/com/example/ridewithme/LoginActivity.java
@@ -29,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("account");
 
-
+        //***********johny***********//
 //        login_EDT_username.setOnClickListener(fillAccount);
 //        login_EDT_password.setOnClickListener(fillAccount);
 
