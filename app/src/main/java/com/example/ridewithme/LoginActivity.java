607ece72b5dbed7bf1d240ee6ca669f5d8b86865
johny <yonatani94@gmail,com>
2020-10-06 23:12:03 +0300

    johny

--- app/src/main/java/com/example/ridewithme/LoginActivity.java
+++ app/src/main/java/com/example/ridewithme/LoginActivity.java
@@ -47,7 +47,6 @@ public class LoginActivity extends AppCompatActivity {
         //***********johny***********//
         login_BTN_login.setOnClickListener(fillAccount);
         login_BTN_register.setOnClickListener(fillAccount);
-        //***********johny***********//
 
 
 
