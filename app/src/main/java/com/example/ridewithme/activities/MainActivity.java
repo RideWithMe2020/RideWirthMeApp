0a487b6832c93334e3ed9d058a8b20d7b260c02b
johny <yonatani94@gmail,com>
2020-10-22 23:37:23 +0300

    johny

--- app/src/main/java/com/example/ridewithme/activities/MainActivity.java
+++ app/src/main/java/com/example/ridewithme/activities/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.example.ridewithme.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,7 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
